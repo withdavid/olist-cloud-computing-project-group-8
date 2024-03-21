1f689b1a3da9ad71b200923a84c8d200bc78e0a0
withdavid <DavidPentester@gmail.com>
2024-03-21 14:37:41 +0000

    added orders table

---
+++ src/ms_orders/utils/sql/orders.sql
@@ -0,0 +1,10 @@
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(20),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME
+);
