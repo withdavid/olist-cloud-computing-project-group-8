ddf23b5d3d6df16af27f74334e511cd6e3879f7f
withdavid <DavidPentester@gmail.com>
2024-04-03 12:53:41 +0100

    fix container name

--- deploy_ms.bat
+++ deploy_ms.bat
@@ -1,12 +1,12 @@
 @echo off
 
 rem Constroi imagens Docker para cada serviço
-docker-compose -f src\ms_order\docker-compose.yml build
+docker-compose -f src\ms_orders\docker-compose.yml build
 @REM docker-compose -f src\ms_JESSE_NOME_MICROSERVICO\docker-compose.yml build
 @REM docker-compose -f src\ms_JOSE_NOME_MICROSERVICO\docker-compose.yml build
 
 rem Iniciar os containers para cada serviço
-docker-compose -f src\ms_order\docker-compose.yml up -d
+docker-compose -f src\ms_orders\docker-compose.yml up -d
 @REM docker-compose -f src\ms_JESSE_NOME_MICROSERVICO\docker-compose.yml up -d
 @REM docker-compose -f src\ms_JOSE_NOME_MICROSERVICO\docker-compose.yml up -d
 
@@ -15,6 +15,6 @@ timeout /t 10 /nobreak >nul
 
 rem Executar migrações de banco de dados (substitua este comando pelo comando real de migração)
 rem Exemplo:
-rem docker-compose -f src\ms_order\docker-compose.yml exec api python manage.py migrate
+rem docker-compose -f src\ms_orders\docker-compose.yml exec api python manage.py migrate
 
 echo Building and deployment completed successfully.
