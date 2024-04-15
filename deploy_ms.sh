da7902405d69dba363e71692b3f5f74de1ccd01f
JamoPT <jamo.oliveira.pt@gmail.com>
2024-04-15 01:19:10 +0100

    feat: add kubernets autoscaling for each service

--- deploy_ms.sh
+++ deploy_ms.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Configure your shell to use the same Docker as Minikube
+eval $(minikube docker-env)
+
 # Constroi imagens Docker para cada serviço
 docker-compose -f src/ms_orders/docker-compose.yml build
 docker-compose -f src/ms_products/docker-compose.yml build
@@ -14,12 +17,17 @@ docker-compose -f src/ms_customers/docker-compose.yml up -d
 sleep 10
 
 # Executar migrações de banco de dados (substitua este comando pelo comando real de migração)
-python src\ms_orders\utils\parse_dataset.py
+python src/ms_orders/utils/parse_dataset.py
+python src/ms_products/utils/parse_dataset.py
+python src/ms_customers/utils/parse_dataset.py
 
 echo "Building and deployment completed successfully."
 
 
 kubectl apply -f src/ms_orders/k8s/deployment.yaml
+kubectl apply -f src/ms_orders/k8s/ms-orders-hpa.yaml
 kubectl apply -f src/ms_products/k8s/deployment.yaml
+kubectl apply -f src/ms_products/k8s/ms-products-hpa.yaml
 kubectl apply -f src/ms_customers/k8s/deployment.yaml
+kubectl apply -f src/ms_customers/k8s/ms-customers-hpa.yaml
 kubectl apply -f k8s/deployment.yaml
